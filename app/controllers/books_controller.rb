512533c51e85c10d64deabece987d2d601c20b24
elmo0509 <elmo.boof@gmail.com>
2021-09-13 07:37:06 +0000

    [Add]bookの新規登録機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,19 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
   def edit
   end
+  
+  # ストロングパラムズ
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
