f3a83a8855e4f153cadb1b2fcd39084c1ba1aca7
elmo0509 <elmo.boof@gmail.com>
2021-09-13 09:20:43 +0000

    [Add]コントローラー・View完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # ルートディレクトリのルーティング
   root 'homes#index'
-  
+
   # resourcesを使用してルーティング設定
   resources :books
-  
+
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
