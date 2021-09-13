c0d8fab5f85aa78dfcd422e07c4230752e3b1bf5
elmo0509 <elmo.boof@gmail.com>
2021-09-13 07:22:55 +0000

    [Add]Topレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # ルートディレクトリのルーティング
+  root 'homes#index'
+  
+  # resourcesを使用してルーティング設定
+  resources :books
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
