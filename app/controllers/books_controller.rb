de9746776a684c6716f56726be859bb7867a2044
elmo0509 <elmo.boof@gmail.com>
2021-09-14 04:53:31 +0000

    [Add]エラー文解決

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -16,16 +17,17 @@ class BooksController < ApplicationController
     if @book.save
       # フラッシュメッセージ　createが成功した時
       flash[:notice] = "Book was successfully created."
-     redirect_to book_path(@book.id)
+     redirect_to book_path(@book)
     else
-      render :new
+      @books = Book.all
+      render ("books/index")
     end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -41,8 +43,8 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to books_path
   end
-  
-  # ストロングパラムズ
+
+  # ストロングパラメータ
   private
   def book_params
     # params.require(:book).permit(:title, :body)とすると「存在しない」か「空」というエラーになる
